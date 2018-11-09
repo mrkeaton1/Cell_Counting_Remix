7faa5539c3c4241f374793b983147b3c936fee36
mmorehea <mmorehea@mix.wvu.edu>
2018-11-09 16:43:57 -0500

    refactored to methods, fixed tiff finding method, added natural sorting for ordering files, use 'python SimpleCellCount.py testtiffs/' to try it

--- SimpleCellCount.py
+++ SimpleCellCount.py
@@ -11,28 +11,48 @@
 import numpy as np
 import os
 import tifffile as tf
+import sys
 from numpy import empty
 from numpy import hstack
 # from PIL import Image
+import re
+import glob
+
+def naturalSort(l): 
+    convert = lambda text: int(text) if text.isdigit() else text.lower() 
+    alphanum_key = lambda key: [ convert(c) for c in re.split('([0-9]+)', key) ] 
+    return sorted(l, key = alphanum_key)
 
 # * Doesn't check if directory exists *
-# def load_tiff():
-tiffAList = []
-nameList = []
-fileCount = 0
+def findTiffsInPath(tiffDirectory):
+	#tiffAList = []
+	#nameList = []
+	#fileCount = 0
+	if not os.path.isdir(tiffDirectory):
+		print("can't find directory")
+		return
 
-tiffDirectory = input("Provide the address of the directory containing the tiff files to open >> ")
-print(tiffDirectory)
+	#instead of using input, use sys.argv to pass parameters into a script via command line
+	#tiffDirectory = input("Provide the address of the directory containing the tiff files to open >> ")
+	print("Loading all .tif files from " + tiffDirectory)
 
-for filename in os.listdir(tiffDirectory):
-    if filename.endswith(".tif") or filename.endswith(".tiff"):
-        #
-        filepath = tiffDirectory + "/" + filename
-        readIm = tf.imread(filepath)
-        tiffAList.append(readIm)
-        nameList.append(filename)
-    else:
-        print("File", filename, "does not have the .tif or .tiff file extension.")
-nameList.sort()
-for name in nameList:
-    print(name)
+	#for filename in os.listdir(tiffDirectory):
+	#	if filename.endswith(".tif") or filename.endswith(".tiff"):
+	#		#
+	#		filepath = tiffDirectory + "/" + filename
+	#		readIm = tf.imread(filepath)
+	#		tiffAList.append(readIm)
+	#		nameList.append(filename)
+	#	else:
+	#	print("File", filename, "does not have the .tif or .tiff file extension.")
+	
+	nameList = glob.glob(tiffDirectory + "*.tif*")
+	naturalSort(nameList)
+	for name in nameList:
+		print(name)
+
+	return nameList
+		
+if __name__ == '__main__':
+	fileToSearch = sys.argv[1]
+	tiffNamesList = findTiffsInPath(fileToSearch);
