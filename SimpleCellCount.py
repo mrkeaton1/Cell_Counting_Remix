9dbadb59161158ca1cc12ced0d9e20ba2b3e1492
Michael Morehead <mmorehea@mix.wvu.edu>
2018-11-09 16:14:05 -0500

    First implementation of load_tiff (in main, not a method)
    Initial implementation of step 1 and step 2 - importing and loading a .tif stack into a numpy array. Currently, each tif stack is a numpy array, but each image is saved in a list.
    
    *One current issue: Files are selected in seemingly random order at this point, and are thus being added to the list in random order.

--- SimpleCellCount.py
+++ SimpleCellCount.py
@@ -4,12 +4,35 @@
 # 2. Loading the .tif stack into a numpy array
 # 3. Importing landmarks - .marker files, typically obtained from the Vaa3D software
 # 4. Checking each landmark to ensure it corresponds to a cell
-# 5. Extracating feature vectors from the landmarks **
+# 5. Extracting feature vectors from the landmarks **
 # 6. Training the model - SVM, sklearn?
 # 7. Test on the remaining points - k-fold testing
-from PIL import Image
 
+import numpy as np
+import os
+import tifffile as tf
+from numpy import empty
+from numpy import hstack
+# from PIL import Image
+
+# * Doesn't check if directory exists *
 # def load_tiff():
+tiffAList = []
+nameList = []
+fileCount = 0
 
-newIm = Image.open('New Stack/3-weeks post-siso EdU every 6 hours PINK 6_z000.tif')
-newIm.show()
+tiffDirectory = input("Provide the address of the directory containing the tiff files to open >> ")
+print(tiffDirectory)
+
+for filename in os.listdir(tiffDirectory):
+    if filename.endswith(".tif") or filename.endswith(".tiff"):
+        #
+        filepath = tiffDirectory + "/" + filename
+        readIm = tf.imread(filepath)
+        tiffAList.append(readIm)
+        nameList.append(filename)
+    else:
+        print("File", filename, "does not have the .tif or .tiff file extension.")
+nameList.sort()
+for name in nameList:
+    print(name)
